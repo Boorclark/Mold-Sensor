9d8d3637af852ccf6640ce78eba232a62fea18a2
Boorclark <clarkg@berea.edu>
2023-03-16 19:00:54 -0400

    Redid code tof follow the format of T04 (untested)

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import os
 import time
+import csv
+import time
 import Adafruit_DHT
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+class MoldSensor:
+    def __init__(self):
+        print("Initializing Mold Sensor Rig")
+        self.DHT_SENSOR = Adafruit_DHT.DHT22
+        self.DHT_PIN = 13
+        self.filename = "data.csv"
 
-try:
-    f = open('/home/pi/humidity.csv', 'a+')
-    if os.stat('/home/pi/humidity.csv').st_size == 0:
-            f.write('Date,Time,Temperature,Humidity\r\n')
-except:
-    pass
+    def start(self):
+        while True:
+            humidity, temperature = Adafruit_DHT.read_retry(self.DHT_SENSOR, self.DHT_PIN)
+            current_time = time.strftime("%m-%d %H:%M", time.localtime())
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+            with open(self.filename, mode='a+', newline='') as data_file:
+                data_writer = csv.writer(data_file)
+                if os.stat(self.filename).st_size == 0:
+                    data_writer.writerow(["Time", "Temperature", "Humidity", "Air Quality"])
+        
+            data_writer.writerow(current_time, "Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        time.sleep(30)
 
-    if humidity is not None and temperature is not None:
-        f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity))
-    else:
-        print("Failed to retrieve data from humidity sensor")
 
-    time.sleep(30)
+
