823d01dc5f3a17bb7e387bfc6502c1a2b4397f2f
Boorclark <clarkg@berea.edu>
2023-03-21 20:34:36 -0400

    DUst sensor working and correct data code

--- main.py
+++ main.py
@@ -10,34 +10,39 @@ class MoldSensor:
         print("Initializing Mold Sensor Rig")
         self.DHT_SENSOR = Adafruit_DHT.DHT22
         self.DHT_PIN = 13
+        self.DUST_PIN = 21
         self.ADC_PIN = 3
         self.GAIN = 1  # Set the gain to 1 for reading voltages up to 4.096V
-        self.filename = "data.csv"
-
-        # Create an ADS1115 ADC instance
         self.adc = Adafruit_ADS1x15.ADS1115()
-
+        self.filename = "data.csv"
+       
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.DHT_PIN, GPIO.IN)
+        GPIO.setup(self.DUST_PIN, GPIO.IN)
 
     def start(self):
+        # Create a new "Data" folder if it doesn't exist
+        if not os.path.exists("Data"):
+            os.makedirs("Data")
+
+        # Generate a unique file name with current time
+        current_time = time.strftime("%m-%d-%Y_%H:%M:%S", time.localtime())
+        file_name = "Data/data_{}.csv".format(current_time)
+
         while True:
             humidity, temperature = Adafruit_DHT.read_retry(self.DHT_SENSOR, self.DHT_PIN)
-            
-            # Read the analog voltage from the ADS1115 on A3
+
             voltage = self.adc.read_adc(self.ADC_PIN, gain=self.GAIN) / 32767.0 * 4.096  # Convert the raw ADC reading to voltage
-            airQuality = voltage  # Assign the voltage reading to airQuality
+            airQuality = 170 * voltage - 0.1   # Convert the voltage to particulate matter concentration in μg/m³ (based on PMS5003 datasheet)
 
-            current_time = time.strftime("%m-%d %H:%M", time.localtime())
-
-            with open(self.filename, mode='a+', newline='') as data_file:
+            with open(file_name, mode='a+', newline='') as data_file:
                 data_writer = csv.writer(data_file)
-                if os.stat(self.filename).st_size == 0:
-                    data_writer.writerow(["Time", "Temperature", "Humidity", "Air Quality"])
-                print(['{0}'.format(current_time), 'Temp={0:0.1f}*C'.format(temperature), 'Humidity={0:0.1f}%'.format(humidity*3), 'Air Quality= {0:0.1f}V'.format(airQuality)])
-                data_writer.writerow(['{0}'.format(current_time), 'Temp={0:0.1f}*C'.format(temperature), 'Humidity={0:0.1f}%'.format(humidity*3), 'Air Quality= {0:0.1f}V'.format(airQuality)])
-            time.sleep(30)
-            
+                # Write header row only if the file is newly created
+                if data_file.tell() == 0:
+                    data_writer.writerow(["Time", "Temperature(c)", "Humidity(%)", "Air Quality(μg/m3)"])
+                print(['{0}'.format(current_time), 'Temp={0:0.1f}*C'.format(temperature), 'Humidity={0:0.1f}%'.format(humidity*3), 'Air Quality= {0}μg/m3'.format(airQuality)])
+                data_writer.writerow(['{0}'.format(current_time), 'Temp={0:0.1f}*C'.format(temperature), 'Humidity={0:0.1f}%'.format(humidity*3), 'Air Quality= {0}μg/m3'.format(airQuality)])
+            time.sleep(5)
+
 
 if __name__ == "__main__":
     ms = MoldSensor()
